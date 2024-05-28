2501fb406c9fd8b8f68fda40c4d56f4cf57af09a
B-Pandey <chitawan2018@gmail.com>
2024-05-28 18:33:15 -0400

    add leetcode problems

---
+++ src/main/java/org/leetcode/Main.java
@@ -0,0 +1,50 @@
+package org.leetcode;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Challenge starts...");
+        String one = "abc";
+        String two = "xyz";
+        String mergedString = mergeStringsAlternately(one, two);
+        System.out.println("Result "+ mergedString);
+        System.out.println("Result " + mergeString(one, two));
+    }
+            /*
+             Merge Strings Alternately
+             You are given two strings word1 and word2. Merge the strings by adding letters in
+             alternating order, starting with word1. If a string is longer than the other,
+             append the additional letters onto the end of the merged string.
+             */
+    public static String mergeStringsAlternately(String one, String two){
+        int m = one.length(), n = two.length();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < m || i < n; ++i) {
+            if (i < m) {
+                ans.append(one.charAt(i));
+            }
+            if (i < n) {
+                ans.append(two.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+    public static String mergeString(String firstWord, String secondWord){
+        String mergeWord = "";
+        int m = firstWord.length();
+        int n = secondWord.length();
+        int i = 0;
+        int j = 0;
+
+       while(i<m && j<n){
+           mergeWord+=firstWord.charAt(i++);
+           mergeWord+=secondWord.charAt(j++);
+       }
+       if(i<m){
+           mergeWord+=firstWord.substring(i);
+       }
+       if(j<n){
+           mergeWord+=secondWord.substring(j);
+       }
+        
+        return mergeWord;
+    }
+}
